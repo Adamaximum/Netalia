8a5b98ffcde8088bf6266ce6cc98457f406aed0c
Adam Goren <amg1090@nyu.edu>
2020-12-14 05:36:06 -0500

    Added room transition triggers

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform currentPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPosition = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Switch()
+    {
+        this.transform.position = currentPosition.transform.position;
+    }
+}
---
+++ Assets/Scripts/RoomTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+    public CameraManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.Find("Main Camera").GetComponent<CameraManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            manager.currentPosition.transform.position = this.transform.position;
+            manager.Switch();
+        }
+    }
+}
