2cd6cb1024de982a920d004c3ee4c4a3884092db
Kyra <klz245@nyu.edu>
2021-03-14 17:37:17 -0400

    patrol enemy prototyped
    patrol enemy working. Death state still not created, but player-enemy collision fires. Also added a ProjectileScript to destroy old projectiles.

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        StartCoroutine(WaitThenDestroy());
+    }
+
+    IEnumerator WaitThenDestroy()
+    {
+        yield return new WaitForSeconds(1.2f);
+        Destroy(gameObject);
+    }
+}
