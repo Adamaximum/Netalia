b427ff508c6cfa5628d54fe72a4f16704c132c97
Kyra <klz245@nyu.edu>
2021-04-07 00:20:43 -0400

    audio tweaks

--- Assets/Scripts/Enemies/PatrolEnemyY.cs
+++ Assets/Scripts/Enemies/PatrolEnemyY.cs
@@ -8,7 +8,7 @@ public class PatrolEnemyY : MonoBehaviour
     
     public Vector2 startCoords, endCoords;
     private Vector2 restartCoords;
-    public float speed;
+    public static float speed;
 
     private Rigidbody2D rb;
     private Vector2 xBounds, yBounds;
