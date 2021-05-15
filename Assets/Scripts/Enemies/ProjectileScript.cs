3f74570b03f1524fd9cbca374264966e589c77c6
Kyra <klz245@nyu.edu>
2021-05-15 02:16:43 -0400

    projectiles are destroyed when leaving room
    raycast to check when projectile leaves room

--- Assets/Scripts/Enemies/ProjectileScript.cs
+++ Assets/Scripts/Enemies/ProjectileScript.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class ProjectileScript : MonoBehaviour
 {
     public static bool destroyAll = false;
+    private Collider2D assignedRoom;
+    
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.layer == 8)
@@ -19,7 +21,27 @@ public class ProjectileScript : MonoBehaviour
     {
         if (destroyAll)
             Destroy(gameObject);
+    }
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.25f, LayerMask.GetMask("Rooms"));
+    private void FixedUpdate()
+    {
+        if (assignedRoom == null)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.25f, LayerMask.GetMask("Rooms"));
+
+            if (hit != null)
+            {
+                assignedRoom = hit.collider;
+            }
+        }
+        else
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.25f, LayerMask.GetMask("Rooms"));
+
+            if (hit != null && hit.collider != assignedRoom)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
