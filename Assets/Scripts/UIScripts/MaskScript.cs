f6fe44d2dcc9d1ca91162c8082ed33544c65a614
Kyra <klz245@nyu.edu>
2021-05-15 15:48:39 -0400

    new death transition

--- Assets/Scripts/UIScripts/MaskScript.cs
+++ Assets/Scripts/UIScripts/MaskScript.cs
@@ -6,28 +6,40 @@ public class MaskScript : MonoBehaviour
 {
     private SpriteRenderer sprite;
     public float secondsToFlash;
-    private float flashIncrement;
+    private float flashIncrements;
     
     void Awake()
     {
         sprite = gameObject.GetComponent<SpriteRenderer>();
 
-        flashIncrement = secondsToFlash / Time.deltaTime;
+        secondsToFlash = secondsToFlash*Time.deltaTime;
+        flashIncrements = secondsToFlash / 100;
+
+        gameObject.transform.localScale = new Vector3(Screen.width, Screen.height);
+        sprite.color = new Color(1, 1, 1, 0);
     }
 
     
     public void Flash()
     {
-        /*
-        for (float i = 0; i < 1; i += flashIncrement)
+        StartCoroutine(FlashCoroutine());
+    }
+
+    IEnumerator FlashCoroutine()
+    {
+        for (float i = 0; i < 1; i += .05f)
         {
+            yield return new WaitForSeconds(.01f);
             sprite.color = new Color(1, 1, 1, i);
         }
         
-        for (float i = 1; i > 0; i -= flashIncrement)
+        for (float i = 1; i > 0; i -= .05f)
         {
+            yield return new WaitForSeconds(.01f);
             sprite.color = new Color(1, 1, 1, i);
         }
-        */
+        
+        GameManager.Instance.EndReset();
+        GameManager.Instance.EnablePlayer();
     }
 }
