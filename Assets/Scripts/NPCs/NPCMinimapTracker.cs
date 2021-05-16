6116a8a371866ff6d88258bafdc1192d8ac1d368
Kyra <klz245@nyu.edu>
2021-05-16 03:57:00 -0400

    minimap marker tweaks
    NPC markers disable after conversation

--- Assets/Scripts/NPCs/NPCMinimapTracker.cs
+++ Assets/Scripts/NPCs/NPCMinimapTracker.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class NPCMinimapTracker : MonoBehaviour
 {
     public int orderInGame;
-    private SpriteRenderer minimapMarker;
+    public SpriteRenderer minimapMarker;
 
     private void Awake()
     {
