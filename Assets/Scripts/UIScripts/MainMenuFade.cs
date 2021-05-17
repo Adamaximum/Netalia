20867b858f3b8ccd9171dc938551af680b054bfa
Kyra <klz245@nyu.edu>
2021-05-17 04:39:27 -0400

    main menu fade added

---
+++ Assets/Scripts/UIScripts/MainMenuFade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuFade : MonoBehaviour
+{
+    private Image fadeImg;
+
+    private void Start()
+    {
+        fadeImg = GetComponent<Image>();
+
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        for (float i = 1; i > 0; i -= .01f)
+        {
+            yield return new WaitForSeconds(.005f);
+            fadeImg.color = new Color(0, 0, 0, i);
+        }
+    }
+    
+    private IEnumerator FadeOut()
+    {
+        for (float i = 0; i < 1; i += .01f)
+        {
+            yield return new WaitForSeconds(.005f);
+            fadeImg.color = new Color(0, 0, 0, i);
+        }
+    }
+}
