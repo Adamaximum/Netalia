11c029974734646e3f85fb4bc32aaeefdf82e186
Adam Goren <amg1090@nyu.edu>
2021-05-20 05:08:37 -0400

    Beginning of modifications

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public Button playButton;
+    public Button controlsButton;
+    public Button creditsButton;
+    public Button quitButton;
+    
+    void Start()
+    {
+        playButton.onClick.AddListener(Play);
+        controlsButton.onClick.AddListener(Controls);
+        creditsButton.onClick.AddListener(Credits);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    void Play()
+    {
+
+    }
+
+    void Controls()
+    {
+
+    }
+
+    void Credits()
+    {
+
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
